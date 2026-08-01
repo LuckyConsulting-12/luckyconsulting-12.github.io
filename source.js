85018775da2c5fd0a1180e0edf17d4d6f88fac34
LuckyConsulting-12 <strikestalker098@gmail.com>
2026-08-02 03:16:23 +0700

    Update source.js

--- source.js
+++ source.js
@@ -2,4 +2,4 @@
   TEMPEL LINK WEB APP APPS SCRIPT DI BAWAH INI.
   Contoh: https://script.google.com/macros/s/AKfy.../exec
 */
-window.LUCKY_SHEET_URL = "TEMPEL_LINK_APPS_SCRIPT_DI_SINI";
+window.LUCKY_SHEET_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnQkRoHSqfopbbV6a0gXz1v5bLwb43ENCUbbQ5FSnD22dFywFhC22xXVF7-5TaBmqnIOwBxqDfnOiMWgnW2_Q3cMK7fgvAopGTh-82G7C-Kw_U16llGjSHg55Qa0kojRKANL79Xtqx86AG7eJGWRjskUf_Nks-pEk2hFxWo9NazAHJ681pi42k1K4jMj0uVhY_1M0qPKbzP4n1F1jajrIpnh3Ibpw_GpqIO3iah9K_pc8v-EEXpAnkugRbwvN8oFkRczTi9OJUP365SnJkHouWtwQYKHgA&lib=MyKD_yT8nHvJR_mKmEfnywcRizkqQfNF4";
